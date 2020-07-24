822eff753c52d12a621f7116ec33010d5c4e6d36
Martin Svejda <svejdam13@gmail.com>
2020-07-24 21:37:18 +0100

    update

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const more = document.getElementById('more');
 
 const apiURL = 'https://api.lyrics.ovh';
 
+// Search by song or artist
+async function searchSongs(term) {
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
+
+  console.log(data);
+  showData(data);
+}
+
+// Show song and artist in DOM
+function showData(data) {
+  
+}
+
 // Event listeners
 form.addEventListener('submit', e => {
   e.preventDefault();
